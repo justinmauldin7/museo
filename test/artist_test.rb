4fa76f364d15d06f82bd8aeebe8006bbd6df3257
Justin Mauldin <justin.mauldin7@gmail.com>
2018-10-31 09:18:51 -0600

    Adds working Artist class to finish iteration 1.

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -4,7 +4,7 @@ require 'minitest/pride'
 
 class ArtistTest < Minitest::Test
 
-  def test_a_artist_exists
+  def test_an_artist_exists
     attributes = {
       id: "2",
       name: "Ansel Adams",
@@ -14,5 +14,21 @@ class ArtistTest < Minitest::Test
     }
     artist = Artist.new(attributes)
     assert_instance_of Artist, artist
+  end
+
+  def test_an_artist_has_atributes
+    attributes = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist = Artist.new(attributes)
+    assert_equal "2", artist.id
+    assert_equal "Ansel Adams", artist.name
+    assert_equal "1902", artist.born
+    assert_equal "1984", artist.died
+    assert_equal "United States", artist.country
   end
 end
